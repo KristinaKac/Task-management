9ef348d40c94aeb12166656b84186131276c24be
Kristina Kachalkova <kachalkovakristina@mail.ru>
2024-01-29 19:57:33 +0300

    1.0.1

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Image Manager
-
-[![Build status](https://ci.appveyor.com/api/projects/status/3e5soiu13lyrxj5w?svg=true)](https://ci.appveyor.com/project/KristinaKac/events-image-manager)
-
-https://kristinakac.github.io/events_Image_Manager/
+# Drag and Drop
--- src/components/task_block/task_block.js
+++ src/components/task_block/task_block.js
@@ -8,6 +8,13 @@ export default class TaskBlock {
         this.onCloseAddCardClick = this.onCloseAddCardClick.bind(this);
         this.onAddCardClick = this.onAddCardClick.bind(this);
         this.onremoveTaskClick = this.onremoveTaskClick.bind(this);
+        this.onDragStart = this.onDragStart.bind(this);
+        this.onDragDrop = this.onDragDrop.bind(this);
+        this.onDragDropBlock = this.onDragDropBlock.bind(this);
+        // this.onMouseDown = this.onMouseDown.bind(this);
+        // this.onMouseUp = this.onMouseUp.bind(this);
+        // this.onMouseOver = this.onMouseOver.bind(this);
+
     }
     render() {
         return `
@@ -26,17 +33,87 @@ export default class TaskBlock {
         </div>
         `
     }
+    static setCurrentTask(currentTask) {
+        this.currentTask = currentTask;
+    }
+    static getCurrentTask() {
+        return this.currentTask;
+    }
+    static setCurrentBlock(currentBlock) {
+        this.currentBlock = currentBlock;
+    }
+    static getCurrentBlock() {
+        return this.currentBlock;
+    }
+
     bindToDOM() {
         const taskBlock = this.render();
         this.parentEl.insertAdjacentHTML('beforeend', taskBlock);
         this.dataFromLocalStorage();
         this.addEvents();
+
+        const block = this.parentEl.querySelector('.tasks');
+
+        block.addEventListener('dragover', this.onDragOverBlock);
+        block.addEventListener('dragleave', this.onDragLeaveBlock);
+        block.addEventListener('dragend', this.onDragEndBlock);
+        block.addEventListener('drop', this.onDragDropBlock);
+    }
+    onDragOverBlock(e) {
+        e.preventDefault();
+        e.target.closest('.tasks').style.boxShadow = '0 4px 3px grey';
+    }
+    onDragLeaveBlock(e) {
+        e.target.closest('.tasks').style.boxShadow = 'none';
+    }
+    onDragEndBlock(e) {
+        e.target.closest('.tasks').style.boxShadow = 'none';
+    }
+    onDragDropBlock(e) {
+        e.preventDefault();
+
+        if (this.tasks.length !== 0) { return }
+
+        const id = TaskBlock.getCurrentTask().id;
+        const value = TaskBlock.getCurrentTask().querySelector('span').innerText;
+        
+        this.tasks.push({
+            id: id,
+            text: value,
+        });
+
+        this.removeLocalStorage();
+        this.updateLocalStorage();
+
+        const data = JSON.parse(localStorage.getItem(this.title));
+
+        data.forEach(item => {
+            const taskList = this.parentEl.querySelector('.task_list');
+            const task = this.createTask(item.id, item.text);
+            taskList.appendChild(task);
+
+            // this.tasks.push({
+            //     id: task.id,
+            //     text: task.querySelector('span').innerText,
+            // });
+
+            const btnRemoveTask = task.querySelector('.remove_task');
+            btnRemoveTask.addEventListener('click', this.onremoveTaskClick);
+
+            const t = document.getElementById(task.id);
+            t.addEventListener('dragstart', this.onDragStart);
+            t.addEventListener('dragend', this.onDragEnd);
+            t.addEventListener('dragover', this.onDragOver);
+            t.addEventListener('dragenter', this.onDragEnter);
+            t.addEventListener('drop', this.onDragDrop);
+            t.addEventListener('dragleave', this.onDragLeave);
+        });
     }
 
     dataFromLocalStorage() {
         const taskList = this.parentEl.querySelector('.task_list');
-
-        if (JSON.parse(localStorage.getItem(this.title)).length === 0) {
+        if (!JSON.parse(localStorage.getItem(this.title)) ||
+            JSON.parse(localStorage.getItem(this.title)).length === 0) {
             return;
         }
         const data = JSON.parse(localStorage.getItem(this.title));
@@ -51,6 +128,14 @@ export default class TaskBlock {
 
             const btnRemoveTask = task.querySelector('.remove_task');
             btnRemoveTask.addEventListener('click', this.onremoveTaskClick);
+
+            const t = document.getElementById(task.id);
+            t.addEventListener('dragstart', this.onDragStart);
+            t.addEventListener('dragend', this.onDragEnd);
+            t.addEventListener('dragover', this.onDragOver);
+            t.addEventListener('dragenter', this.onDragEnter);
+            t.addEventListener('drop', this.onDragDrop);
+            t.addEventListener('dragleave', this.onDragLeave);
         });
     }
 
@@ -95,9 +180,116 @@ export default class TaskBlock {
         input.value = '';
 
         const btnRemoveTask = task.querySelector('.remove_task');
-
         btnRemoveTask.addEventListener('click', this.onremoveTaskClick);
+
+        const container = document.querySelector('.container');
+        // container.addEventListener('mousedown', this.onMouseDown)
+
+        const t = document.getElementById(id);
+        t.addEventListener('dragstart', this.onDragStart);
+        t.addEventListener('dragend', this.onDragEnd);
+        t.addEventListener('dragover', this.onDragOver);
+        t.addEventListener('dragenter', this.onDragEnter);
+        t.addEventListener('drop', this.onDragDrop);
+        t.addEventListener('dragleave', this.onDragLeave);
+
     }
+    onDragStart(e) {
+        this.tasks = this.tasks.filter(task => e.target.id !== task.id);
+
+        TaskBlock.setCurrentTask(e.target);
+        TaskBlock.setCurrentBlock(this.tasks);
+        this.removeLocalStorage();
+        this.updateLocalStorage();
+
+        setTimeout(() => {
+            const taskItems = Array.from(this.parentEl.querySelectorAll('.task_item'));
+            taskItems.forEach(task => task.remove());
+        }, 0);
+
+        setTimeout(() => {
+            const data = JSON.parse(localStorage.getItem(this.title));
+
+            data.forEach(item => {
+                const taskList = this.parentEl.querySelector('.task_list');
+
+                const task = this.createTask(item.id, item.text);
+                taskList.appendChild(task);
+
+                const btnRemoveTask = task.querySelector('.remove_task');
+                btnRemoveTask.addEventListener('click', this.onremoveTaskClick);
+
+                const t = document.getElementById(task.id);
+                t.addEventListener('dragstart', this.onDragStart);
+                t.addEventListener('dragend', this.onDragEnd);
+                t.addEventListener('dragover', this.onDragOver);
+                t.addEventListener('dragenter', this.onDragEnter);
+                t.addEventListener('drop', this.onDragDrop);
+                t.addEventListener('dragleave', this.onDragLeave);
+            });
+        }, 100);
+    }
+    onDragEnd(e) {
+        e.target.style.boxShadow = 'none';
+        e.target.classList.remove('hidden');
+    }
+    onDragOver(e) {
+        e.preventDefault();
+        // console.log('over')
+        if (e.target.classList.contains('task_item')) {
+            e.target.style.boxShadow = '0 4px 3px grey';
+        }
+    }
+    onDragLeave(e) {
+        // console.log('leave')
+        e.target.style.boxShadow = 'none';
+    }
+    onDragEnter() {
+        // console.log('enter')
+    }
+    onDragDrop(e) {
+        e.preventDefault();
+
+        const id = TaskBlock.getCurrentTask().id;
+        const value = TaskBlock.getCurrentTask().querySelector('span').innerText;
+
+        const dropElementIndex = this.tasks.findIndex(task => e.target.id === task.id);
+
+        this.tasks.splice((dropElementIndex + 1), 0, { id: id, text: value });
+
+        const taskItems = Array.from(this.parentEl.querySelectorAll('.task_item'));
+        taskItems.forEach(task => task.remove());
+
+        this.removeLocalStorage();
+        this.updateLocalStorage();
+
+        const data = JSON.parse(localStorage.getItem(this.title));
+
+        data.forEach(item => {
+            const taskList = this.parentEl.querySelector('.task_list');
+            const task = this.createTask(item.id, item.text);
+            taskList.appendChild(task);
+
+            // this.tasks.push({
+            //     id: task.id,
+            //     text: task.querySelector('span').innerText,
+            // });
+
+            const btnRemoveTask = task.querySelector('.remove_task');
+            btnRemoveTask.addEventListener('click', this.onremoveTaskClick);
+
+            const t = document.getElementById(task.id);
+            t.addEventListener('dragstart', this.onDragStart);
+            t.addEventListener('dragend', this.onDragEnd);
+            t.addEventListener('dragover', this.onDragOver);
+            t.addEventListener('dragenter', this.onDragEnter);
+            t.addEventListener('drop', this.onDragDrop);
+            t.addEventListener('dragleave', this.onDragLeave);
+        });
+
+        // this.dataFromLocalStorage();
+    }
+
     onremoveTaskClick(e) {
         const item = e.currentTarget.closest('.task_item');
         this.tasks = this.tasks.filter(task => item.id !== task.id);
@@ -109,6 +301,7 @@ export default class TaskBlock {
         const li = document.createElement('li');
         li.className = 'task_item';
         li.id = id;
+        li.draggable = true;
 
         const span = document.createElement('span');
         span.innerText = value;
